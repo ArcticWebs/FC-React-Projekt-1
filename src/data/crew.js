2ba5cb5a7110b40f7981eda663a47b9d0637fe3f
Ania Kiedrowska <kiedrowska.ania@gmail.com>
2024-02-21 19:42:13 +0100

    repo structure fixed

--- src/data/crew.js
+++ src/data/crew.js
@@ -1,14 +1,17 @@
+import spec1 from "assets/spec1.jpg";
+import spec2 from "assets/spec2.jpg";
+
 export const crew = [
   {
     name: "Robert Makłowicz [serwis i usługi]",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel massa et lacus egestas cursus a non magna. Fusce scelerisque blandit nunc, id malesuada ex lobortis a. Integer felis nisi, tempor elementum lorem in, varius pellentesque ligula. Duis efficitur lacinia enim, non tincidunt libero ultrices in.",
-    imgSrc: "/assets/spec1.jpg",
+    imgSrc: spec1,
   },
   {
     name: "Małgorzata Rozenek [marketing]",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel massa et lacus egestas cursus a non magna. Fusce scelerisque blandit nunc, id malesuada ex lobortis a. Integer felis nisi, tempor elementum lorem in, varius pellentesque ligula. Duis efficitur lacinia enim, non tincidunt libero ultrices in.",
-    imgSrc: "/assets/spec2.jpg",
+    imgSrc: spec2,
   },
 ];
